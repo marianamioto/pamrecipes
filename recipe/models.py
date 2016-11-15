b1c7d6fa4ef4642eada2238c60d751b78c7a92f3
Mariana Mioto <mariana.mioto@gmail.com>
2016-11-15 20:45:07 -0200

    added admin to recipe

--- recipe/models.py
+++ recipe/models.py
@@ -6,3 +6,6 @@ class Recipe(models.Model):
     name = models.CharField(max_length=80)
     instructions = models.TextField()
     author = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.name

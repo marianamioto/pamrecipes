bfd45e99b72d626157adf1f6e28b4fc116575f36
Mariana Mioto <mariana.mioto@gmail.com>
2016-11-15 20:26:15 -0200

    created a model for recipe

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=80)
+    instructions = models.TextField()
+    author = models.ForeignKey(User)
